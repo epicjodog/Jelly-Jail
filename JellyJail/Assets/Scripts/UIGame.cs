271c22e90ef14359c819585cc7480a3d691c43af
KotoUllia <91286309+KotoUllia@users.noreply.github.com>
2022-12-23 02:03:41 -0500

    s t u f f

--- JellyJail/Assets/Scripts/UIGame.cs
+++ JellyJail/Assets/Scripts/UIGame.cs
@@ -14,12 +14,16 @@ public class UIGame : MonoBehaviour
 
     [Header("Health")]
     public Slider healthMeter;
+    public GameObject heartDead;
+    public GameObject heartAlive;
 
     [Header("Inputs")]
-    public int currentHealth;
+    private int currentHealth;
     public int maxHealth;
     
-    
+    GlobalController globalController; // currentHealth, maxBullets
+    PlayerMovement playerMovement; // 
+
 
     void Start()
     {
@@ -28,23 +32,86 @@ public class UIGame : MonoBehaviour
             ammoArsenal[ammoCount].SetActive(true);
             ammoCount++;
         }
+        currentAmmo = ammoCount;
+        print(currentAmmo);
+
+        currentHealth = globalController.currentHealth;
 
         healthMeter.value = currentHealth;
+        healthMeter.maxValue = maxHealth;
+
+        maxAmmo = globalController.maxBullets;
     }
 
-    public void SetHealth(int recievedHealth)
+    /////////////////////////////////////////////////////////////////////////////////
+    // Health Functions
+    public void HealthSetCurrent(int recievedHealth)
     {
+        if(currentHealth == 0)
+        {
+            heartDead.SetActive(true);
+            heartAlive.SetActive(false);
+        }
+
         healthMeter.value = recievedHealth;
     }
 
+    public void HealthSetMax(int recievedMaxHP)
+    {
+        healthMeter.maxValue = recievedMaxHP;
+    }
+
+    public void HealthRevive()
+    {
+        // Resets back to full HP and moves out of death icon
+
+        heartDead.SetActive(false);
+        heartAlive.SetActive(true);
+        currentHealth = maxHealth;
+        healthMeter.value = maxHealth;
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////
+    // Ammo Stuff
+    public void AmmoLoss()
+    {
+        ammoArsenal[currentAmmo].GetComponent<Image>().color = new Color(0, 0, 0, 1);
+        currentAmmo--;
+    }
+    public void AmmoGain()
+    {
+        ammoArsenal[currentAmmo].GetComponent<Image>().color = new Color(1, 1, 1, 1);
+        currentAmmo++;
+    }
+    
+    public void AmmoInc()
+    {
+        while(ammoCount < maxAmmo)
+        {
+            ammoArsenal[ammoCount].SetActive(true);
+            ammoCount++;
+        }
+        currentAmmo = ammoCount;
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////
+    // Testing Purposes
     public void OnClickHealthLoss()
     {
         currentHealth--;
-        SetHealth(currentHealth);
+        HealthSetCurrent(currentHealth);
     }
     public void OnClickHealthGain()
     {
         currentHealth++;
-        SetHealth(currentHealth);
+        HealthSetCurrent(currentHealth);
+    }
+    public void OnClickAmmoGain()
+    {
+        AmmoGain();
+    }
+    public void OnClickAmmoLoss()
+    {
+        AmmoLoss();
     }
 }
